c440e34c556c24b3fb820b659614d9d340ebe3d1
Thomas Roch <thomas.c.roch@gmail.com>
2015-06-15 16:44:40 +0100

    Add match and build methods

--- index.js
+++ index.js
@@ -4,26 +4,32 @@ Object.defineProperty(exports, '__esModule', {
     value: true
 });
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-var rules = {
-    parameter: /^:([a-zA-Z0-9-_]+)/,
-    delimiter: /^\//,
-    // subDelimiter:
-    fragment: /^(.*?)(?=\/|:)/
-};
+var rules = [{
+    name: 'parameter',
+    pattern: /^:([a-zA-Z0-9-_]+)/
+}, {
+    name: 'delimiter',
+    pattern: /^\//
+}, {
+    name: 'fragment',
+    pattern: /^(.*?)(?=\/|:)/
+}];
 
 var tokenise = function tokenise(str) {
     var tokens = arguments[1] === undefined ? [] : arguments[1];
 
-    Object.keys(rules).some(function (rule) {
-        var match = str.match(rules[rule]);
+    rules.some(function (rule) {
+        var match = str.match(rule.pattern);
         if (!match) return false;
 
         tokens.push({
-            type: rule,
+            type: rule.name,
             match: match[0],
-            val: match.slice(1)
+            val: match.length > 1 ? match.slice(1) : null
         });
 
         tokenise(str.substr(match[0].length), tokens);
@@ -33,14 +39,31 @@ var tokenise = function tokenise(str) {
     return tokens;
 };
 
-var Path = function Path(path) {
-    _classCallCheck(this, Path);
+var Path = (function () {
+    function Path(path) {
+        _classCallCheck(this, Path);
 
-    if (!path) throw new Error('Please supply a path');
-    this.path = path;
-    this.tokens = tokenise(path);
-};
+        if (!path) throw new Error('Please supply a path');
+        this.path = path;
+        this.tokens = tokenise(path);
+    }
+
+    _createClass(Path, [{
+        key: 'match',
+        value: function match() {}
+    }, {
+        key: 'build',
+        value: function build() {
+            var params = arguments[0] === undefined ? {} : arguments[0];
+        }
+    }]);
+
+    return Path;
+})();
 
 exports['default'] = Path;
 module.exports = exports['default'];
+// regex:   /a/
+// regex:   /\//
+// regex:   /\//
 

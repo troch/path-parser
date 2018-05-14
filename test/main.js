adc36b040ad86162946b51b0f0862538559fa086
Thomas Roch <thomas.c.roch@gmail.com>
2018-05-14 12:23:28 +0100

    feat: support  sign in parameters

--- test/main.js
+++ test/main.js
@@ -280,4 +280,12 @@ describe('Path', function() {
             param: '1|2'
         })
     })
+
+    it('should support a wide range of characters', () => {
+        const path = new Path('/test/:param')
+
+        path.test('/test/1+2=3').should.eql({
+            param: '1+2=3'
+        })
+    })
 })
